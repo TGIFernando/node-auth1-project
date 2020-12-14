4e0db856511219faa8a6247f9a38124bfddd75b6
Fernando Chavez <fjchavez01@gmail.com>
2020-12-14 14:20:36 -0800

    got log in working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -15,6 +15,20 @@ router.post('/register', M.checkPayload, M.checkUsernameUnique, async (req, res)
     }
 })
 
+router.post('/login',  M.checkPayload, M.checkUsernameExists, (req, res) => {
+    try {
+        const verify = bcrypt.compareSync(req.body.password, req.userData.password)
+        if (verify) {
+            req.session.user = req.userData
+            res.json(`Logged in`)
+        } else {
+            res.status(401).json('You shall not pass!')
+        }
+    } catch (e) {
+        res.status(500).json({ message: e.message})
+    }
+})
+
 
 
 module.exports = router
